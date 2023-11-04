26aa2f1ce03fb05df716af29ff9b9773d0abdf8f
Eduardo <gamingtechedu@gmail.com>
2023-11-04 15:16:19 -0300

    v0.0.21

--- lib/src/controller/tabela.dart
+++ lib/src/controller/tabela.dart
@@ -3,7 +3,7 @@ import 'package:projeto_kdl_flutter/src/controller/table_controller.dart';
 
 class TabelaController extends ChangeNotifier {
   fetch() {
-    print('Dados atualizados da tabela');
+    print('Dados atualizados da tabela1');
     notifyListeners();
   }
 }
